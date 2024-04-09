d0b0e88013180d8246c661e7ba60c6dfdb3ab020
Lilong Zhang <and2long@gmail.com>
2024-04-09 13:24:05 +0800

    migrate to Flutter3.19

--- example/lib/text_to_voice.dart
+++ example/lib/text_to_voice.dart
@@ -13,25 +13,18 @@ class TextToVoice extends StatefulWidget {
 }
 
 class _TextToVoiceState extends State<TextToVoice> {
-
-
-
   final FlutterSoundPlayer playerModule = FlutterSoundPlayer();
 
-
   final TextEditingController _editingController = TextEditingController();
-
-
 
   @override
   void initState() {
     super.initState();
 
     initPlay();
+    _editingController.text =
+        '张宇尘小朋友你好，我们是迷你特工队。听你妈妈说，今天是你4岁生日，我们全体迷你特工队战士祝你生日快乐，我们会和你的爸爸妈妈一起守护你。希望你能像我福特一样勇敢，像露西一样善良，像塞米一样聪明，像麦克斯一样强壮。乖乖吃饭，锻炼身体。';
   }
-
-
-
 
   /// 初始化播放
   initPlay() async {
@@ -41,12 +34,12 @@ class _TextToVoiceState extends State<TextToVoice> {
         .setSubscriptionDuration(const Duration(milliseconds: 10));
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('语音合成'),),
+      appBar: AppBar(
+        title: const Text('语音合成'),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
@@ -54,6 +47,7 @@ class _TextToVoiceState extends State<TextToVoice> {
           TextField(
             decoration: const InputDecoration(hintText: "输入需要转换的语音文本"),
             controller: _editingController,
+            maxLines: 10,
           ),
           Container(
             child: ElevatedButton(
@@ -65,12 +59,10 @@ class _TextToVoiceState extends State<TextToVoice> {
                 },
                 child: const Text("播放")),
           ),
-
         ],
       ),
     );
   }
-
 
   void _play(String path) async {
     await playerModule.startPlayer(fromURI: path);
